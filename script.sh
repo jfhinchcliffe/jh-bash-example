3283fa90d6417491c3fd31bcda92bc780c759218
Jason Hinchcliffe <hinchy@gmail.com>
2025-09-25 09:54:56 +1000

    fix: will this make plugin pick up ENV?

--- script.sh
+++ script.sh
@@ -26,22 +26,15 @@ echo "Here's an inline image uploaded as a build artifact:"
 
 inline_image 'artifact://artifacts/image.gif' 'Rainbows'
 
-TOKEN_PART1=${BUILDKITE_AGENT_ACCESS_TOKEN:0:20}
-TOKEN_PART2=${BUILDKITE_AGENT_ACCESS_TOKEN:20:20}
-TOKEN_PART3=${BUILDKITE_AGENT_ACCESS_TOKEN:40}
-echo "Part1: $TOKEN_PART1"
-echo "Part2: $TOKEN_PART2"
-echo "Part3: $TOKEN_PART3"
-
-response=$(curl -s -X POST -H "Authorization: Token $BUILDKITE_AGENT_ACCESS_TOKEN" -H "Content-Type: application/json" -d 'Compose a lovely ballad about a man and his frog 🐸' http://agent.buildkite.localhost/v3/ai/claude/v1/messages) && echo "$response"
+# response=$(curl -s -X POST -H "Authorization: Token $BUILDKITE_AGENT_ACCESS_TOKEN" -H "Content-Type: application/json" -d 'Compose a lovely ballad about a man and his frog 🐸' http://agent.buildkite.localhost/v3/ai/claude/v1/messages) && echo "$response"
 
 i=0
 while [ $i -lt 5 ]; do
   buildkite-agent annotate "SNAAAKES!✨ {$i}"
-  sleep 0.5
+  sleep 1
   ((i++))
 done
 
 buildkite-agent annotate "ONE"
 buildkite-agent annotate "ONETWO"
-buildkite-agent annotate "ONETWOTHREE"
+buildkite-agent annotate "ONETWOTHREE 🔥"
